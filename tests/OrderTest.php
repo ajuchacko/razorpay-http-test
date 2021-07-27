1676b30f5796b3b5017dc9a8e99fd2ea56e452be
aju <ajuchacko91@gmail.com>
2021-07-28 01:02:24 +0530

    creaes new test suite

---
+++ tests/OrderTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ajuchacko\RazorpayHttp\Tests;
+
+use Ajuchacko\RazorpayHttp\FakeRazorpay;
+use Ajuchacko\RazorpayHttp\Order;
+
+class OrderTest extends TestCase
+{
+	function setUp(): void
+	{
+		parent::setUp();
+    	
+    	$this->razorpay = app('razorpay');
+	}
+
+    /** @test */
+    function it_can_create_a_test_order()
+    {
+    	$order  = $this->razorpay->order->create(
+    		[
+    			'amount' => 100, 
+    			'currency' => 'INR',
+    			'receipt' => '123', 
+    			'notes' => ['key' => 'custom order note'],
+    			'partial_payment' => false
+    		]
+		);
+
+    	$this->assertInstanceOf(Order::class, $order);
+    	$this->assertFalse($order->partial_payment);
+    	$this->assertEquals(100, $order->amount);
+    	$this->assertEquals(100, $order->amount_due);
+    	$this->assertEquals(0, $order->amount_paid);
+    	$this->assertEquals(0, $order->attempts);
+    	$this->assertEquals('created', $order->status);
+    	$this->assertEquals('123', $order->receipt);
+    	$this->assertEquals(['key' => 'custom order note'], $order->notes);
+    }
+}
--- tests/RazorpayHttpTest.php
+++ tests/RazorpayHttpTest.php
@@ -19,28 +19,4 @@ class RazorpayHttpTest extends TestCase
     {
     	$this->assertInstanceOf(FakeRazorpay::class, $this->razorpay);
     }
-
-    /** @test */
-    function it_can_create_a_test_order()
-    {
-    	$order  = $this->razorpay->order->create(
-    		[
-    			'amount' => 100, 
-    			'currency' => 'INR',
-    			'receipt' => '123', 
-    			'notes' => ['key' => 'custom order note'],
-    			'partial_payment' => false
-    		]
-		);
-
-    	$this->assertInstanceOf(Order::class, $order);
-    	$this->assertFalse($order->partial_payment);
-    	$this->assertEquals(100, $order->amount);
-    	$this->assertEquals(100, $order->amount_due);
-    	$this->assertEquals(0, $order->amount_paid);
-    	$this->assertEquals(0, $order->attempts);
-    	$this->assertEquals('created', $order->status);
-    	$this->assertEquals('123', $order->receipt);
-    	$this->assertEquals(['key' => 'custom order note'], $order->notes);
-    }
 }
