3dda1cb1298f9acb30ad2f3100ca1bf1392ba5a5
aju <ajuchacko91@gmail.com>
2021-08-01 20:25:46 +0530

    get config properly

--- src/RazorpayHttpServiceProvider.php
+++ src/RazorpayHttpServiceProvider.php
@@ -22,8 +22,9 @@ class RazorpayHttpServiceProvider extends PackageServiceProvider
     public function packageRegistered()
     {
         $this->app->singleton(Api::class, function ($app) {
-           	
-         	[$api_key, $api_secret] = array_values(config('razorpay-http'));
+
+            $api_key = config('razorpay-http.api_key');
+            $api_secret = config('razorpay-http.api_secret');
 
         	if ($app->environment() == 'testing') {
 	            return new FakeApi($api_key, $api_secret);
